cd75d5e3063261fdd37d6aed5462fb1189eb38ca
Boris Shminke <boris@shminke.ml>
2022-05-09 09:57:47 +0200

    flake8 instead of pycodestyle only

--- local-build.sh
+++ local-build.sh
@@ -5,7 +5,7 @@ PACKAGE_NAME=gym_saturation
 cd doc
 make clean html
 cd ..
-pycodestyle --max-doc-length 10000 --ignore E203,E501,W503 ${PACKAGE_NAME}
+flake8 ${PACKAGE_NAME}
 pylint --rcfile=.pylintrc ${PACKAGE_NAME}
 mypy --config-file mypy.ini ${PACKAGE_NAME}
 pytest --cov-report term-missing
