0aa02256abca6f5567dfc1ca1f75c6843b7f6bcd
Boris Shminke <boris@shminke.com>
2023-08-07 17:54:33 +0200

    add vulnerabilities check

--- local-build.sh
+++ local-build.sh
@@ -5,10 +5,12 @@ PACKAGE_NAME=gym_saturation
 cd doc
 make clean html
 cd ..
-pydocstyle ${PACKAGE_NAME}
-flake8 ${PACKAGE_NAME}
-pylint ${PACKAGE_NAME}
-mypy ${PACKAGE_NAME}
+pydocstyle ${PACKAGE_NAME} scripts
+flake8 ${PACKAGE_NAME} scripts
+pylint ${PACKAGE_NAME} scripts
+mypy ${PACKAGE_NAME} scripts
 pytest --cov-report term-missing
 pyroma .
+bandit -r ${PACKAGE_NAME}
+bandit -r scripts
 scc -i py ${PACKAGE_NAME}
