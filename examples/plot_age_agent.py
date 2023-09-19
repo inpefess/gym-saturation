264561c0260ff2f0052b3df9daf516cab5009f92
Boris Shminke <boris@shminke.com>
2023-09-19 22:33:48 +0200

    update tutorial

--- examples/plot_age_agent.py
+++ examples/plot_age_agent.py
@@ -28,25 +28,11 @@ observation, info = env.reset()
 print(info)
 
 # %%
-# observation is a dictionary with two keys
-
-print(observation.keys())
-
-# %%
-# ``real_obs`` value is a JSON representation of logic clauses
+# observation is a tuple of JSON representations of logic clauses
 
 import pprint
 
-from gym_saturation.constants import ACTION_MASK, REAL_OBS
-
-pprint.pp(observation[REAL_OBS])
-
-# %%
-# ``action_mask`` is a ``numpy`` array of zeros and ones
-
-print(type(observation[ACTION_MASK]))
-print(observation[ACTION_MASK].shape)
-print(observation[ACTION_MASK][:10])
+pprint.pp(observation)
 
 # %%
 # We can render the environment state in the TPTP format.
@@ -56,11 +42,13 @@ print(observation[ACTION_MASK][:10])
 env.render()
 
 # %%
-# here is an example of an episode during which we play random avail actions
+# here is an example of an episode during which we play random actions.
+# We set the random seed for reproducibility.
 
+env.action_space.seed(0)
 terminated, truncated = False, False
 while not (terminated or truncated):
-    action = env.action_space.sample(mask=observation[ACTION_MASK])
+    action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
 env.close()
 
@@ -73,15 +61,7 @@ print(terminated, truncated, reward)
 # It means we arrived at a contradiction (``$false``) which proves the lemma.
 # Notice the ``birth_step`` number of a contradiction, it shows how many steps
 # we did to find proof.
-pprint.pp(observation[REAL_OBS][-1])
-
-# %%
-# the package also provides a utility function for extracting only clauses
-# which became parts of the proof (some steps might be unnecessary to find the
-# proof)
-from gym_saturation.utils import get_tstp_proof
-
-print(get_tstp_proof(observation[REAL_OBS]))
+pprint.pp(observation[-1])
 
 # %%
 # Age agent for iProver
@@ -93,19 +73,18 @@ env = gym.make("iProver-v0")
 
 # %%
 # Instead of a random agent, let's use Age agent which selects actions in the
-# order they became available
+# order they appear
 
 observation, info = env.reset()
 terminated, truncated = False, False
 action = 0
 while not (terminated or truncated):
-    if observation[ACTION_MASK][action] == 1:
-        observation, reward, terminated, truncated, info = env.step(action)
+    observation, reward, terminated, truncated, info = env.step(action)
     action += 1
 env.close()
 
 # %%
-# We still arrive at the contradiction but it might take a different number of
-# steps. And the proof found looks a bit different
+# We still arrive at contradiction but it takes a different number of steps
 
-print(get_tstp_proof(observation[REAL_OBS]))
+print(terminated, truncated, reward)
+pprint.pp(observation[-1])
