8aa7c62c6c38d77aa6405923ace2ef9bd0a134af
Boris Shminke <boris@shminke.com>
2023-09-23 20:04:01 +0200

    remove unnecessary lines

--- doc/conf.py
+++ doc/conf.py
@@ -12,45 +12,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Sphinx config."""
 # pylint: disable-all
-# -- Path setup --------------------------------------------------------------
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-# -- Project information -----------------------------------------------------
 project = "gym-saturation"
 version = "0.10.5"
 copyright = "2021-2023, Boris Shminke"
 author = "Boris Shminke"
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     # uncomment to rebuild examples
     # "sphinx_gallery.gen_gallery",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "furo"
 html_title = "gym-saturation documentation"
 sphinx_gallery_conf = {
