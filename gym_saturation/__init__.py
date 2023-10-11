18cb83ce289dcc6cd1ed101ddaa416af8f139447
Boris Shminke <boris@shminke.com>
2023-10-11 16:05:43 +0200

    Bump to 0.11.1

--- gym_saturation/__init__.py
+++ gym_saturation/__init__.py
@@ -22,4 +22,4 @@ from gymnasium.envs.registration import register
 register(id="Vampire-v0", entry_point="gym_saturation.envs:VampireEnv")
 register(id="iProver-v0", entry_point="gym_saturation.envs:IProverEnv")
 register(id="Vampair-v0", entry_point="gym_saturation.envs:VampairEnv")
-__version__ = "0.11.0"
+__version__ = "0.11.1"
