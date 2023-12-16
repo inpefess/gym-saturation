19b267f2e30b92394c6439d41a95fe2dfaba6888
Boris Shminke <boris@shminke.com>
2023-12-16 13:04:43 +0100

    Bump to 0.11.3

--- doc/conf.py
+++ doc/conf.py
@@ -20,7 +20,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 project = "gym-saturation"
-version = "0.11.2"
+version = "0.11.3"
 copyright = "2021-2023, Boris Shminke"
 author = "Boris Shminke"
 extensions = [
