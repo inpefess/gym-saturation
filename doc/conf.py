f56d76785af69da7b0e07a827d2d5f852dd3400f
Boris Shminke <boris@shminke.com>
2025-06-01 17:04:29 +0200

    use project metadata

--- doc/conf.py
+++ doc/conf.py
@@ -15,17 +15,17 @@
 
 """Sphinx config."""
 
-import os
-import sys
+import gym_saturation
+from importlib.metadata import distribution
 
-sys.path.insert(0, os.path.abspath(".."))
-project = "gym-saturation"
-version = "1.0.1"
-copyright = "2021-2025, Boris Shminke"
-author = "Boris Shminke"
+distribution_metadata = distribution(gym_saturation.__name__).metadata
+project = distribution_metadata["Name"]
+version = distribution_metadata["Version"]
+author = distribution_metadata["Author"]
+copyright = f"2021-2025, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
 ]
 html_theme = "furo"
-html_title = "gym-saturation documentation"
+html_title = f"{project} documentation"
--- gym_saturation/__init__.py
+++ gym_saturation/__init__.py
@@ -22,4 +22,3 @@ from gymnasium.envs.registration import register
 
 register(id="Vampire-v0", entry_point="gym_saturation.envs:VampireEnv")
 register(id="iProver-v0", entry_point="gym_saturation.envs:IProverEnv")
-__version__ = "1.0.1"
